3a8122c7cc774252fee58fed800adbec5ed4c648
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2023-10-04 01:42:13 +0200

    Init repository

---
+++ src/Inherit.jl
@@ -0,0 +1,44 @@
+module Inherit
+
+export @inherit
+
+function count_unionall_components(@nospecialize(t))
+    i = 0
+    while isa(t, UnionAll)
+        t = t.body
+        i += 1
+    end
+    return i
+end
+
+macro inherit(expr)
+    parentname = if expr isa Symbol
+        expr
+    elseif expr.head == :curly
+        expr.args[1]
+    else
+        throw(ArgumentError("unknown expression"))
+    end
+
+    parent = getproperty(__module__, parentname)
+    isstructtype(parent) || throw(ArgumentError("parent type must be a struct"))
+
+    if parent isa UnionAll
+        expr isa Expr || throw(ArgumentError("parent type is a parametric type but no type parameters are defined"))
+        typevars = expr.args[2:end]
+        count_unionall_components(parent) == length(typevars) ||
+            throw(ArgumentError("not enough type parameters"))
+        parent = parent{map(TypeVar, typevars)...}
+    end
+
+    # instrospect parent type
+    fields = map(fieldnames(parent), fieldtypes(parent)) do name, type
+        :($name::$type)
+    end
+
+    quote
+        $(esc.(fields)...)
+    end
+end
+
+end
